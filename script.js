fba7f31325cb13803feda2991bb27efa44c34deb
kokivasileski <thekoki001@gmail.com>
2022-02-12 13:08:17 -0500

    merge

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-// create app object for Pokedex
- 
-// create a method to initialize the app
-
-// create a method to request info from the API
-const fetchPokemon = function() {
-    for (let i = 1; i <= 40; i++) {
-// create variables for the API to hold:
-// use URL constructor
-// fetch data and convert to JSON
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-    fetch(url)
-        .then(function(result) {
-            return result.json()
-        })
-        .then(function(newResult) {
-            const pokemon = {
-                name: newResult.name,
-                image: newResult.sprites,
-                type: newResult.types
-            }
-            console.log(pokemon)
-        })
-    }
-}
-
-fetchPokemon()
-
-// create a user input that will display pokemon from API
-    // create an event listener 
-// use the DOM to create li elements
-// use the DOM to create img elements
-// use the DOM to create p elements
-// populate li elements with images from the API
-// get pokemon description from the API and append it to p elements
-
-// ************************************************
-
-=======
 const pokedex = {};
 pokedex.init = function(){
 
@@ -210,4 +170,3 @@ pokedex.init()
 // };
 
 // fetchPokemon();
->>>>>>> 1998f3792eda53111a58e144fbb4d39ccaa9e5c5
