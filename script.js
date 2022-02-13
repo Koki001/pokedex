6ca20b5d37201610db6e243566d354e45718bdb0
kokivasileski <thekoki001@gmail.com>
2022-02-13 13:28:38 -0500

    fixed button functionality and added styles

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const pokedex = {};
 pokedex.init = function(){
-   pokedex.fetchPokemon()
+    pokedex.fetchPokemon()
 }
-
-const pokeGreeting = ["Hey!","What's up!", "Hello!", "YO!", "Hi!", "Good Day!" ]
+console.log(pokedex)
+const pokeGreeting = ["Hey!", "What's up!", "Hello!", "YO!", "Hi!", "Good Day!", "How's it going?", "Greetings!"]
 const randomGreeting = pokeGreeting[Math.floor(Math.random() * pokeGreeting.length)]
-
-
 
 pokedex.fetchPokemon = function () {
     // for (let i = 1; i <= 10; i++) {
-    const random = Math.floor(Math.random() * 800)
+    const random = Math.floor(Math.random() * 850 + 1)
     // create variables for the API to hold:
     // use URL constructor
     // fetch data and convert to JSON
@@ -24,6 +22,7 @@ pokedex.fetchPokemon = function () {
                 name: newResult.name,
                 image: newResult.sprites.other.dream_world['front_default'],
                 imageTwo: newResult.sprites['front_default'],
+                imageThree: newResult.sprites.other["official-artwork"]['front_default'],
                 type: newResult.types[0].type.name,
                 id: newResult.id,
                 height: newResult.height,
@@ -33,7 +32,6 @@ pokedex.fetchPokemon = function () {
                 attack: newResult.stats[1].base_stat,
 
             }
-            console.log(newResult.height)
             pokedex.displayPokemon = function () {
                 const greetTitle = document.querySelector('.greetingTitle');
                 const pId = document.querySelector('.pId');
@@ -43,25 +41,24 @@ pokedex.fetchPokemon = function () {
                 const pHp = document.querySelector('.pHp');
                 const pAttack = document.querySelector('.pAttack');
                 const pDefense = document.querySelector('.pDefense');
-                
-
-            
                 const div = document.querySelector('.singlePokemonContainer')
                 const image = document.createElement('img')
 
                 if (pokemon.image == null) {
+                    image.src = pokemon.imageThree
+                } else if (pokemon.imageThree == null) {
                     image.src = pokemon.imageTwo
                 } else {
                     image.src = pokemon.image
                 }
                 pId.innerHTML = `ID: ${pokemon.id}`
-                pHeight.innerHTML = `Height: ${pokemon.height}`
-                pWeight.innerHTML = `Weight: ${pokemon.weight}`
+                pHeight.innerHTML = `Height: ${pokemon.height / 10}m`
+                pWeight.innerHTML = `Weight: ${pokemon.weight / 10}kg`
                 pType.innerHTML = `Type: ${pokemon.type}`
                 pHp.innerHTML = `HP: ${pokemon.hp}`
                 pAttack.innerHTML = `Attack: ${pokemon.attack}`
                 pDefense.innerHTML = `Defense: ${pokemon.defense}`
-                greetTitle.innerHTML = `${randomGreeting} I'm ${pokemon.name}`
+                greetTitle.innerHTML = `${randomGreeting} I'm ${pokemon.name}.`
                 div.appendChild(image)
             }
 
@@ -73,9 +70,8 @@ pokedex.fetchPokemon = function () {
 
 // create a method to request info from the API
 const singleButton = document.querySelector('.singlePokemonButton')
-console.log("button", singleButton)
 singleButton.addEventListener('click', function (refresh) {
-console.log("button clicked", singleButton)
+
 refresh.preventDefault()
     function clear() {
         document.querySelector('.singlePokemonContainer').innerHTML = "";
@@ -83,7 +79,7 @@ refresh.preventDefault()
     clear()
     pokedex.fetchPokemon = function () {
     // for (let i = 1; i <= 10; i++) {
-        const random = Math.floor(Math.random()* 800)
+        const random = Math.floor(Math.random() * 850 + 1)
         // create variables for the API to hold:
         // use URL constructor
         // fetch data and convert to JSON
@@ -97,22 +93,50 @@ refresh.preventDefault()
                     name: newResult.name,
                     image: newResult.sprites.other.dream_world['front_default'],
                     imageTwo: newResult.sprites['front_default'],
+                    imageThree: newResult.sprites.other["official-artwork"]['front_default'],
                     type: newResult.types[0].type.name,
                     id: newResult.id,
+                    height: newResult.height,
+                    weight: newResult.weight,
+                    hp: newResult.stats[0].base_stat,
+                    defense: newResult.stats[2].base_stat,
+                    attack: newResult.stats[1].base_stat,
+
                 }
+                const randomGreeting = pokeGreeting[Math.floor(Math.random() * pokeGreeting.length)]
                 pokedex.displayPokemon = function() {
                     const div = document.querySelector('.singlePokemonContainer')
                     const image = document.createElement('img')
+                    const greetTitle = document.querySelector('.greetingTitle');
+                    const pId = document.querySelector('.pId');
+                    const pHeight = document.querySelector('.pH');
+                    const pWeight = document.querySelector('.pW');
+                    const pType = document.querySelector('.pType');
+                    const pHp = document.querySelector('.pHp');
+                    const pAttack = document.querySelector('.pAttack');
+                    const pDefense = document.querySelector('.pDefense');
+                    image.classList.add("fadeImg")
 
-                    if (pokemon.image == null){
+                    if (pokemon.image == null) {
+                        image.src = pokemon.imageThree
+                    } else if (pokemon.imageThree == null) {
                         image.src = pokemon.imageTwo
-                    } else{
+                    } else {
                         image.src = pokemon.image
                     }
-                    const meetTitle = document.querySelector('.greetingTitle')
-                    meetTitle.innerHTML =  `Hey my name is ${pokemon.name}`
+                    
+                    
+                    pId.innerHTML = `ID: ${pokemon.id}`
+                    pHeight.innerHTML = `Height: ${pokemon.height / 10}m`
+                    pWeight.innerHTML = `Weight: ${pokemon.weight / 10}kg`
+                    pType.innerHTML = `Type: ${pokemon.type}`
+                    pHp.innerHTML = `HP: ${pokemon.hp}`
+                    pAttack.innerHTML = `Attack: ${pokemon.attack}`
+                    pDefense.innerHTML = `Defense: ${pokemon.defense}`
+                    greetTitle.innerHTML = `${randomGreeting} I'm ${pokemon.name}.`
 
                     div.appendChild(image)
+
                 }
 
                 pokedex.displayPokemon()
@@ -124,107 +148,94 @@ refresh.preventDefault()
     pokedex.fetchPokemon()
 })
 
+// const checkSubmit = function () {
+//     //If the checkbox has been checked
+//     if (document.getElementById("manyPoke").checked) {
+//         //Set the disabled property to FALSE and enable the button.
+//         document.getElementById("manyPoke").disabled = false;
+//     } else {
+//         document.getElementById("manyPoke").disabled = true;
+//     }
+// }
+
 const form = document.querySelector('form')
 form.addEventListener('submit', function (refresh){
-refresh.preventDefault()
-function clear() {
-    document.querySelector('ul').innerHTML = "";
-}
-clear()
-const input = document.querySelector('input[name = "poke"]:checked').value
-console.log(input)
-pokedex.fetchPokemon = function () {
-    for (let i = 1; i <= input; i++) {
-        const random = Math.floor(Math.random() * 800)
-        // create variables for the API to hold:
-        // use URL constructor
-        // fetch data and convert to JSON
-        const url = `https://pokeapi.co/api/v2/pokemon/${random}`
-        fetch(url)
-            .then(function (result) {
-                return result.json()
-            })
-            .then(function (newResult) {
-                const pokemon = {
-                    name: newResult.name,
-                    image: newResult.sprites.other.dream_world['front_default'],
-                    imageTwo: newResult.sprites['front_default'],
-                    type: newResult.types[0].type.name,
-                    // typeTwo: newResult.types[1].type.name,
-                    id: newResult.id,
-                }
-                
-                pokedex.displayPokemon = function () {
-                    const ul = document.querySelector('ul')
-                    const li = document.createElement('li')
-                    const title = document.createElement('h3')
-                    const image = document.createElement('img')
-                    const pokeId = document.createElement('p')
-                    const type = document.createElement('h5')
-
-
-                    if (pokemon.image == null) {
-                        image.src = pokemon.imageTwo
-                    } else {
-                        image.src = pokemon.image
-                    }
-
-                    title.innerHTML = pokemon.name
-                    pokeId.innerHTML =`#${pokemon.id}`
-                    type.innerHTML = pokemon.type
-                    ul.appendChild(li)
-                    li.appendChild(image)
-                    li.appendChild(pokeId)
-                    li.appendChild(title)
-                    li.appendChild(type)
-
-                }
-
-                pokedex.displayPokemon()
-                console.log(newResult)
-            })
+    
+    refresh.preventDefault()
+    
+    function clear() {
+        document.querySelector('ul').innerHTML = "";
     }
-}
-pokedex.fetchPokemon()
+    clear()
+
+    let inputCheckState = document.querySelector('input[name = "poke"]')
+    if (inputCheckState = document.querySelector('input[name = "poke"]:checked')) {
+        const input = document.querySelector('input[name = "poke"]:checked').value
+        pokedex.fetchPokemon = function () {
+            for (let i = 1; i <= input; i++) {
+                const random = Math.floor(Math.random() * 850 + 1)
+                // create variables for the API to hold:
+                // use URL constructor
+                // fetch data and convert to JSON
+                const url = `https://pokeapi.co/api/v2/pokemon/${random}`
+                fetch(url)
+                    .then(function (result) {
+                        return result.json()
+                    })
+                    .then(function (newResult) {
+                        const pokemon = {
+                            name: newResult.name,
+                            image: newResult.sprites.other.dream_world['front_default'],
+                            imageTwo: newResult.sprites['front_default'],
+                            imageThree: newResult.sprites.other["official-artwork"]['front_default'],
+                            type: newResult.types[0].type.name,
+                            // typeTwo: newResult.types[1].type.name,
+                            id: newResult.id,
+                        }
+                        
+                        pokedex.displayPokemon = function () {
+                            const ul = document.querySelector('ul')
+                            const li = document.createElement('li')
+                            const title = document.createElement('h3')
+                            const image = document.createElement('img')
+                            const pokeId = document.createElement('p')
+                            const type = document.createElement('h5')
+    
+    
+                            if (pokemon.image == null) {
+                                image.src = pokemon.imageThree
+                            } else if (pokemon.imageThree == null) {
+                                image.src = pokemon.imageTwo
+                            } else {
+                                image.src = pokemon.image
+                            }
+    
+                            title.innerHTML = pokemon.name
+                            pokeId.innerHTML =`#${pokemon.id}`
+                            type.innerHTML = pokemon.type
+                            ul.appendChild(li)
+                            li.appendChild(image)
+                            li.appendChild(pokeId)
+                            li.appendChild(title)
+                            li.appendChild(type)
+    
+                        }
+    
+                        pokedex.displayPokemon()
+                        const scrollPoint = document.querySelector(".browseContainer")
+                        scrollPoint.scrollIntoView({
+                            behavior: "smooth",
+                            block: "start"
+                        })
+                        console.log(newResult)
+                    })
+            }
+        }
+        pokedex.fetchPokemon()
+    } else {
+        alert("Please select one of the values above the button before searching! Your options are labelled '4', '8', and '12'.")
+    }
 
 }) 
 
 pokedex.init()
-
-
-// const pokedex = document.getElementById('pokedex');
-
-// const fetchPokemon = () => {
-//     const promises = [];
-//     for (let i = 1; i <= 150; i++) {
-//         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-//         promises.push(fetch(url).then((res) => res.json()));
-//     }
-//     Promise.all(promises).then((results) => {
-//         const pokemon = results.map((result) => ({
-//             name: result.name,
-//             image: result.sprites['front_default'],
-//             type: result.types.map((type) => type.type.name).join(', '),
-//             id: result.id
-//         }));
-//         displayPokemon(pokemon);
-//     });
-// };
-
-// const displayPokemon = (pokemon) => {
-//     console.log(pokemon);
-//     const pokemonHTMLString = pokemon
-//         .map(
-//             (pokeman) => `
-//         <li class="card">
-//             <img class="card-image" src="${pokeman.image}"/>
-//             <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-//             <p class="card-subtitle">Type: ${pokeman.type}</p>
-//         </li>
-//     `
-//         )
-//         .join('');
-//     pokedex.innerHTML = pokemonHTMLString;
-// };
-
-// fetchPokemon();
